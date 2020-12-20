9636639144af00a414c37492055fa7b48040b872
fireteam99 <raymancheungsy@gmail.com>
2020-12-20 15:13:55 -0500

    added more comprehensive tests

--- elevator.py
+++ elevator.py
@@ -10,7 +10,8 @@ class Elevator:
             max_capacity=10,
             load_delay=5,
             lobby_load_delay=30,
-            time=0
+            time=0,
+            verbosity='low'
     ) -> None:
         assert min_floor <= current_floor <= max_floor
         self.max_capacity = max_capacity
@@ -24,18 +25,30 @@ class Elevator:
         self.lobby_load_delay = lobby_load_delay
         self.time = time
         self.direction = 0
+        self.verbosity = verbosity
 
-    def simulate_tick(self, new_calls) -> None:
+    def simulate_tick(self, new_calls, verbosity=None) -> None:
+        """
+        Simulates elevator state transitions for one tick
+        :param new_calls: a list of new Call objects received
+        :param verbosity: the level of logging ('off', 'low', 'high')
+        :return: None
+        """
+
+        if verbosity is None:
+            verbosity = self.verbosity
+
         self.calls += new_calls
 
         # log the internal state
-        print('time:', self.time)
-        print('new calls:', [call.to_string() for call in new_calls])
-        print('state:', self.state)
-        print('current floor:', self.current_floor)
-        print('seconds since state changed:', self.seconds_since_state_changed())
-        print('passengers:', self.passenger_count())
-        print('direction:', self.direction)
+        if verbosity == 'high':
+            print(f'time: {self.time}')
+            print(f'new calls: {[call.to_string() for call in new_calls]}')
+            print(f'state: {self.state}')
+            print(f'current floor: {self.current_floor}')
+            print(f'seconds since state changed: {self.seconds_since_state_changed()}')
+            print(f'passengers: {self.passenger_count()}')
+            print(f'direction: {self.direction}')
 
         if self.state == 'IDLE':
             assert len([call for call in self.calls if call.state == 'BOARDED']) == 0
@@ -47,10 +60,10 @@ class Elevator:
                     key=lambda c: (self.time - c.init_time, abs(self.current_floor - c.origin))
                 )
                 if oldest_closest_open_call.origin == self.current_floor:  # change state to loading
-                    self.transition_loading()
+                    self.transition_loading(verbosity)
                 else:  # head towards the oldest closest open call
-                    self.transition_moving()
-                    print(f'heading towards floor {oldest_closest_open_call.origin}')
+                    self.transition_moving(verbosity)
+                    verbosity != 'off' and print(f'heading towards floor {oldest_closest_open_call.origin}')
                     self.direction = 1 if oldest_closest_open_call.origin > self.current_floor else -1
             else:
                 self.direction = 0
@@ -61,51 +74,57 @@ class Elevator:
             )
 
             # if the elevator is empty treat all calls as going in the same direction
-            calls_at_floor_same_dir = calls_at_floor if len(self.boarded_calls()) == 0 \
-                else [call for call in calls_at_floor if call.direction() == self.direction]
+            calls_at_floor_same_dir = calls_at_floor
+            if len(self.boarded_calls()) > 0:
+                calls_at_floor_same_dir = [call for call in calls_at_floor if call.direction() == self.direction]
 
             # transition into loading if any boarded calls need to exit, or any calls at floor need to board
             if (any(c.destination == self.current_floor for c in self.boarded_calls()) > 0
                     or (len(calls_at_floor_same_dir) > 0 and calls_at_floor_same_dir[
                         0].size < self.current_capacity())):
-                self.transition_loading()
+                self.transition_loading(verbosity)
 
         elif self.state == 'LOADING':
             calls_at_floor = sorted(self.calls_at_floor(self.calls, self.current_floor), key=lambda c: c.size)
             arrived_calls = [call for call in self.boarded_calls() if call.destination == self.current_floor]
 
             # if time is not up, keep doors open, and load/off-load any passengers
-            if (self.current_floor != 1 and self.seconds_since_state_changed() < self.load_delay) \
-                    or (self.current_floor == 1 and self.seconds_since_state_changed() < self.lobby_load_delay):
+            if (self.current_floor != 1 and self.seconds_since_state_changed() <= self.load_delay) \
+                    or (self.current_floor == 1 and self.seconds_since_state_changed() <= self.lobby_load_delay):
 
                 # off-load arrived calls
                 for call in arrived_calls:
                     call.arrive(self.time)
-                    print(f'off-loading call: {call.to_string()} ')
+                    verbosity != 'off' and print(f'off-loading call: {call.to_string()} ')
 
-                # try to update direction if there no calls on board and calls at floor
+                #  update direction if there no calls on board and there are calls at floor
                 if len(calls_at_floor) > 0 and len(self.boarded_calls()) == 0:
                     # find the direction of the oldest call at the floor
                     oldest_call = max(calls_at_floor, key=lambda c: self.time - c.init_time)
                     self.direction = oldest_call.direction()
-                    print(f'updating direction to: {self.direction}')
+                    verbosity != 'off' and print(f'updating direction to: {self.direction}')
 
-                # find any calls at floor that are traveling the same direction as the elevator
-                calls_at_floor_same_dir = [call for call in calls_at_floor if call.direction() == self.direction]
+                # if the elevator is empty treat all calls as going in the same direction
+                calls_at_floor_same_dir = calls_at_floor if len(self.boarded_calls()) == 0 \
+                    else [call for call in calls_at_floor if call.direction() == self.direction]
+
+                # filter the calls by size, then sort by size (increasing to decreasing) - to maximize people loaded
+                calls_at_floor_same_dir = [call for call in calls_at_floor_same_dir if self.passenger_count() + call.size <= self.max_capacity]
+                calls_at_floor_same_dir.sort(key=lambda c: c.size,reverse=True)
 
                 # load as many same direction calls as possible
                 for call in calls_at_floor_same_dir:
                     if call.size + self.passenger_count() <= self.max_capacity:
                         call.board(self.time)
-                        print('loading call:', call.to_string())
+                        verbosity != 'off' and print('loading call:', call.to_string())
                     else:
                         break
 
             else:  # if time is up close doors and transition to moving or idle
                 if len(self.boarded_calls()) > 0:  # if there are people on board continue moving
-                    self.transition_moving()
+                    self.transition_moving(verbosity)
                 else:  # otherwise switch into idle state
-                    self.transition_idle()
+                    self.transition_idle(verbosity)
                     self.direction = 0
 
         # move elevator if necessary
@@ -119,22 +138,22 @@ class Elevator:
 
         # update time
         self.time += 1
-        print()
+        verbosity != 'off' and print()
 
-    def transition_loading(self):
+    def transition_loading(self, verbosity):
         self.state = 'LOADING'
         self.time_last_state_changed = self.time
-        print('transitioning to loading')
+        verbosity != 'off' and print('transitioning to loading')
 
-    def transition_moving(self):
+    def transition_moving(self, verbosity):
         self.state = 'MOVING'
         self.time_last_state_changed = self.time
-        print('transitioning to moving')
+        verbosity != 'off' and print('transitioning to moving')
 
-    def transition_idle(self):
+    def transition_idle(self, verbosity):
         self.state = 'IDLE'
         self.time_last_state_changed = self.time
-        print('transitioning to idle')
+        verbosity != 'off' and print('transitioning to idle')
 
     def is_moving_up(self) -> bool:
         return self.state == 'MOVING' and self.direction == 1
@@ -161,9 +180,10 @@ class Elevator:
     def seconds_since_state_changed(self) -> int:
         return self.time - self.time_last_state_changed
 
+    # returns open calls at a particular floor
     def calls_at_floor(self, calls, floor):
         assert self.min_floor <= floor <= self.max_floor
-        return [call for call in calls if call.origin == floor]
+        return [call for call in calls if call.origin == floor and call.state == 'OPEN']
 
     def call_going_in_direction(self, calls, direction):
         assert -1 <= direction <= 1
@@ -180,7 +200,7 @@ class Elevator:
                 arrived_calls)
             return average_wait_time, average_ride_time
         else:
-            return 'No arrived calls'
+            return None, None
 
 
 class Call:
@@ -203,10 +223,12 @@ class Call:
         return 1 if self.is_going_up() else -1
 
     def board(self, time) -> None:
+        assert self.state == 'OPEN'
         self.state = 'BOARDED'
         self.board_time = time
 
     def arrive(self, time) -> None:
+        assert self.state == 'BOARDED'
         self.state = 'ARRIVED'
         self.arrival_time = time
 
@@ -229,7 +251,7 @@ def simulate(time_series):
     return elevator.stats()
 
 
-def basic_testcases():
+def demos():
     # going up
     # time_series = [[] for _ in range(20)]
     # time_series[0].append(Call(origin=1, destination=2, size=1, init_time=0))
@@ -257,7 +279,7 @@ def basic_testcases():
 
 
 def main():
-    basic_testcases()
+    demos()
 
 
 if __name__ == '__main__':
--- test.py
+++ test.py
@@ -1,7 +1,107 @@
+import sys
 import unittest
-from elevator import Elevator
+from elevator import Elevator, Call
+
 
 class TestCallMethods(unittest.TestCase):
-
     def test_is_going_up(self):
-        pass
+        call = Call(origin=1, destination=5, size=1, init_time=1)
+        self.assertTrue(call.is_going_up())
+
+    def test_is_going_down(self):
+        call = Call(origin=10, destination=5, size=1, init_time=1)
+        self.assertTrue(call.is_going_down())
+
+    def test_direction(self):
+        upcall = Call(origin=1, destination=5, size=1, init_time=1)
+        self.assertEqual(upcall.direction(), 1)
+
+        downcall = Call(origin=10, destination=5, size=1, init_time=1)
+        self.assertEqual(downcall.direction(), -1)
+
+    def test_board(self):
+        call = Call(origin=1, destination=5, size=1, init_time=1)
+        call.board(5)
+        self.assertEqual(call.state, 'BOARDED')
+        self.assertEqual(call.board_time, 5)
+
+    def test_arrive(self):
+        call = Call(origin=1, destination=5, size=1, init_time=1)
+        call.board(5)
+        call.arrive(10)
+        self.assertEqual(call.state, 'ARRIVED')
+        self.assertEqual(call.arrival_time, 10)
+
+    def test_to_string(self):
+        call = Call(origin=1, destination=5, size=1, init_time=1)
+        self.assertEqual(call.to_string(), 'init time: 1, origin: 1, dest: 5, size: 1')
+
+
+class TestElevatorMethods(unittest.TestCase):
+
+    def test_idle_to_loading(self):
+        elevator = Elevator(verbosity="off")
+        elevator.simulate_tick([Call(origin=1, destination=2, size=1, init_time=1)])
+        self.assertEqual(elevator.state, 'LOADING')
+
+    def test_idle_to_moving(self):
+        elevator = Elevator(verbosity="off")
+        elevator.simulate_tick([Call(origin=2, destination=1, size=1, init_time=1)])
+        self.assertEqual(elevator.state, 'MOVING')
+
+    def test_moving_to_loading(self):
+        elevator = Elevator(lobby_load_delay=1, load_delay=1, verbosity="off")
+        elevator.simulate_tick([Call(origin=2, destination=1, size=1, init_time=1)])
+        self.assertEqual(elevator.state, 'MOVING')
+        elevator.simulate_tick([])
+        self.assertEqual(elevator.state, 'LOADING')
+
+    def test_loading_to_moving(self):
+        elevator = Elevator(lobby_load_delay=1, load_delay=1, verbosity="off")
+        elevator.simulate_tick([Call(origin=1, destination=2, size=1, init_time=1)])
+        self.assertEqual(elevator.state, 'LOADING')
+        elevator.simulate_tick([])
+        elevator.simulate_tick([])
+        self.assertEqual(elevator.state, 'MOVING')
+
+    def test_loading_to_idle(self):
+        elevator = Elevator(lobby_load_delay=1, load_delay=1, verbosity="off")
+        elevator.simulate_tick([Call(origin=1, destination=2, size=1, init_time=1)])
+        self.assertEqual(elevator.state, 'LOADING')
+        elevator.simulate_tick([])
+        elevator.simulate_tick([])
+        self.assertEqual(elevator.state, 'MOVING')
+        elevator.simulate_tick([])
+        self.assertEqual(elevator.state, 'LOADING')
+        elevator.simulate_tick([])
+        elevator.simulate_tick([])
+        self.assertEqual(elevator.state, 'IDLE')
+
+    def test_capacity(self):
+        elevator = Elevator(max_capacity=10, lobby_load_delay=1, load_delay=1, verbosity="low")
+        elevator.simulate_tick([
+            Call(origin=1, destination=2, size=9, init_time=1),
+            Call(origin=1, destination=2, size=2, init_time=1),
+        ])
+        self.assertEqual(elevator.state, 'LOADING')
+        elevator.simulate_tick([])
+        self.assertEqual(elevator.state, 'LOADING')
+        elevator.simulate_tick([])
+        self.assertEqual(elevator.state, 'MOVING')
+        self.assertTrue(elevator.passenger_count() <= elevator.max_capacity)
+        elevator.simulate_tick([])
+        self.assertEqual(elevator.state, 'LOADING')
+        elevator.simulate_tick([])
+        self.assertEqual(elevator.state, 'LOADING')
+        elevator.simulate_tick([])
+        self.assertEqual(elevator.state, 'IDLE')
+        self.assertEqual(elevator.passenger_count(), 0)
+        elevator.simulate_tick([])
+        self.assertEqual(elevator.state, 'MOVING')
+        elevator.simulate_tick([])
+        self.assertEqual(elevator.state, 'LOADING')
+
+
+if __name__ == "__main__":
+    suite = unittest.TestLoader().loadTestsFromModule(sys.modules[__name__])
+    unittest.TextTestRunner(verbosity=3).run(suite)
